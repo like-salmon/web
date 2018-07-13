d0236a32a5561cf8dfbc583ca30832526d4c3cbe
lyx <lzh@estronger.cn>
2018-07-13 10:13:16 +0800

    rm all

--- lilian/Application/Home/Model/NewsModel.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Model;
-use Think\Model;
-class NewsModel extends Model {
-    //protected $tableName = 'news';
-    protected $tablePrefix = 'llidc_';//没有表前缀的情况必须设置，否则会获取当前配置文件中的 DB_PREFIX。
-
-}
--- template/wmodules.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-
-import tornado
-class Nav(tornado.web.UIModule):
-    def render(self):
-        return self.render_string("nav.html",active=active)
